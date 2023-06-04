ce6b668c9b799f3cd0ccdf9e840f6511b28fcd01
gongbenzheng <1459490255@qq.com>
2023-06-04 21:26:30 +0800

    微服务模板-算法模块

---
+++ micro-client/src/main/java/com/template/micro/client/controller/AlgorithmController.java
@@ -0,0 +1,36 @@
+package com.template.micro.client.controller;
+
+import com.template.micro.client.utils.EditDistanceUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import utils.vo.Result;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 作者
+ * @since 2023-05-06
+ */
+@Api(tags = {"[算法接口], tags = [算法接口], description = [算法接口]"})
+@RestController
+@RequestMapping("/algorithm")
+public class AlgorithmController {
+    //增删改查
+    @Autowired
+    private EditDistanceUtils editDistanceUtils;
+
+    @ApiOperation(value = "编辑距离-相似度")
+    @GetMapping(value = "/editDistance")
+    public Result<String> editDistance(@RequestParam("content_first") String content_first,@RequestParam("content_second") String content_second) {
+        return new Result<String>().success(editDistanceUtils.similarity(content_first,content_second));
+    }
+
+}
+
